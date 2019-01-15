f0a456596f478fbe53d007eb6d88157ccf128985
andy reyes <11710reyes@gmail.com>
2019-01-15 02:00:03 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,7 +31,11 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, girls|
+    girls.each do |contestant_hash|
+      
+    end
+  end 
 end
 
 def get_average_age_for_season(data, season)
