6ba30e5776ad8a5ecb8811779a8f6712e919dc7f
andy reyes <11710reyes@gmail.com>
2019-01-15 01:46:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,14 +2,15 @@ require "pry"
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant_hash|
     if contestant_hash["status"].downcase == "winner"
-      return contestant_hash["name"].split(" ").last
+      return contestant_hash["name"].split(" ").first
     end
   end
 end
 
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, girls|
+    
 end
 
 def count_contestants_by_hometown(data, hometown)
