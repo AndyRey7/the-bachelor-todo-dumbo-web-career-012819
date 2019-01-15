c4c88cb653c48b221510c4d5cd0369af2c7bdeff
andy reyes <11710reyes@gmail.com>
2019-01-15 01:49:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,6 +13,7 @@ def get_contestant_name(data, occupation)
     girls.each do |contestant_hash|
       if contestant_hash["occupation"] == occupation
         return contestant_hash["name"]
+      end
     end
   end
 end
