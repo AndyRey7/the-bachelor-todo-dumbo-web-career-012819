d62272072ce4ddabb4f3c9d2007e10213c22d66c
andy reyes <11710reyes@gmail.com>
2019-01-15 02:12:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -42,8 +42,9 @@ end
 
 def get_average_age_for_season(data, season)
   age_of_girls = 0 
-  number_of_contestants = 0 
+  count = 0 
   data[season].each do |contestant_hash|
     age_of_girls += (contestant_hash["age"]).to_i
+    count +=1
   end 
 end
