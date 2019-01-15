1c08db2e14e9710868721f702115849886378fb1
andy reyes <11710reyes@gmail.com>
2019-01-15 01:39:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 require "pry"
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant_hash|
-    if contestant_hash[:status] == "winner"
+    if contestant_hash["status"] == "winner"
       return
   end
 end
